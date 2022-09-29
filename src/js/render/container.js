39c0dbefa35974cd7e200c97bd6a9ffc3709a219
Frit027 <Frit_027@mail.ru>
2022-09-29 13:00:39 +0300

    wrapper initialization function for feeds and posts

---
+++ src/js/render/container.js
@@ -0,0 +1,18 @@
+export default (container, name) => {
+  const card = document.createElement('div');
+  card.classList.add('card', 'border-0');
+  container.append(card);
+
+  const h2 = document.createElement('h4');
+  h2.classList.add('card-title', 'h4');
+  h2.textContent = name;
+
+  const cardBody = document.createElement('div');
+  cardBody.classList.add('card-body');
+  cardBody.append(h2);
+  card.append(cardBody);
+
+  const ul = document.createElement('ul');
+  ul.classList.add('list-group', 'border-0', 'rounded-0');
+  card.append(ul);
+};
