a7fb9490ab17ecaa828eba16e0e1864eae23ffbc
Frit027 <Frit_027@mail.ru>
2022-09-30 10:31:37 +0300

    functions moved to other files

--- src/js/index.js
+++ src/js/index.js
@@ -2,13 +2,12 @@ import * as yup from 'yup';
 import onChange from 'on-change';
 import render from './render/index';
 import * as config from './constants';
-import { axiosXML, getFeedWithPosts } from './parser';
+import { getFeed, getPosts } from './parser';
+import axiosXML from './network';
+import setError from './savingError';
+import setTimer from './timer';
 
-const setError = (state, message) => {
-  const watchedState = state;
-  watchedState.additionForm.state = config.formStates.invalid;
-  watchedState.additionForm.errorKey = message;
-};
+const isURLExist = (feeds, url) => feeds.map((feed) => feed.rssURL).includes(url);
 
 export default (state) => {
   const schema = yup.object().shape({
@@ -25,15 +24,17 @@ export default (state) => {
       schema
         .validate({ url: new FormData(e.target).get('url') })
         .then((data) => {
-          if (watchedState.feeds.map((feed) => feed.rssURL).includes(data.url)) {
+          if (isURLExist(watchedState.feeds, data.url)) {
             throw new Error('additionForm.errors.notUnique');
           }
           axiosXML(data.url)
             .then((document) => {
-              const { feed, posts } = getFeedWithPosts(document, data.url);
+              const feed = getFeed(document, data.url);
               watchedState.feeds.push(feed);
-              watchedState.posts.push(...posts);
+              watchedState.posts.push(...getPosts(document, feed.id));
               watchedState.additionForm.state = config.formStates.valid;
+
+              setTimer(watchedState);
             })
             .catch((err) => {
               setError(
