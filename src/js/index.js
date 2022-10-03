2033070190ce6b90d7657b740cc3d9e179870af0
Frit027 <Frit_027@mail.ru>
2022-10-03 12:55:35 +0300

    code moved to function

--- src/js/index.js
+++ src/js/index.js
@@ -1,50 +1,44 @@
-import * as yup from 'yup';
+import '../scss/styles.scss';
+import 'bootstrap';
+import i18next from 'i18next';
 import onChange from 'on-change';
-import render from './render/index';
 import * as config from './constants';
-import { getFeed, getPosts } from './parser';
-import axiosXML from './network';
-import setError from './savingError';
-import setTimer from './timer';
+import app from './app';
+import render from './render';
 
-const isURLExist = (feeds, url) => feeds.map((feed) => feed.rssURL).includes(url);
-
-export default (state) => {
-  const schema = yup.object().shape({
-    url: yup.string().url('additionForm.errors.invalidURL'),
+(async () => {
+  await i18next.init({
+    lng: 'ru',
+    debug: true,
+    resources: {
+      ru: {
+        translation: {
+          additionForm: {
+            errors: {
+              invalidURL: 'Ссылка должна быть валидным URL',
+              invalidRSS: 'Ресурс не содержит валидный RSS',
+              notUnique: 'RSS уже существует',
+              errNetwork: 'Ошибка сети',
+            },
+            info: {
+              uploadedRSS: 'RSS успешно загружен',
+            },
+          },
+        },
+      },
+    },
   });
+
+  const state = {
+    additionForm: {
+      state: config.formStates.valid,
+      errorKey: '',
+    },
+    feeds: [],
+    posts: [],
+    readPostLinkID: null,
+  };
   const watchedState = onChange(state, render);
 
-  document.body
-    .querySelector('form')
-    .addEventListener('submit', (e) => {
-      e.preventDefault();
-      watchedState.additionForm.state = config.formStates.check;
-
-      schema
-        .validate({ url: new FormData(e.target).get('url') })
-        .then((data) => {
-          if (isURLExist(watchedState.feeds, data.url)) {
-            throw new Error('additionForm.errors.notUnique');
-          }
-          axiosXML(data.url)
-            .then((document) => {
-              const feed = getFeed(document, data.url);
-              watchedState.feeds.push(feed);
-              watchedState.posts.push(...getPosts(document, feed.id));
-              watchedState.additionForm.state = config.formStates.valid;
-
-              setTimer(watchedState);
-            })
-            .catch((err) => {
-              setError(
-                watchedState,
-                err.code === 'ERR_NETWORK' ? 'additionForm.errors.errNetwork' : 'additionForm.errors.invalidRSS',
-              );
-            });
-        })
-        .catch((error) => {
-          setError(watchedState, error.message);
-        });
-    });
-};
+  app(watchedState);
+})();
