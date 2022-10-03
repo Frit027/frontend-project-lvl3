543318a35c9f35f4741e79d76e77792adc7b012a
Frit027 <Frit_027@mail.ru>
2022-10-03 12:56:16 +0300

    code moved to function

--- src/js/render/posts.js
+++ src/js/render/posts.js
@@ -1,5 +1,40 @@
 import initContainer from './container';
 
+const createA = ({ id, title, link }) => {
+  const a = document.createElement('a');
+  a.classList.add('fw-bold');
+  a.setAttribute('href', link);
+  a.setAttribute('target', '_blank');
+  a.setAttribute('rel', 'noopener noreferrer');
+  a.setAttribute('data-id', id);
+  a.textContent = title;
+
+  return a;
+};
+
+const createButton = ({
+  id, title, description, link,
+}) => {
+  const button = document.createElement('button');
+  button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
+  button.setAttribute('type', 'button');
+  button.setAttribute('data-bs-toggle', 'modal');
+  button.setAttribute('data-bs-target', '#modal');
+  button.setAttribute('data-id', id);
+  button.textContent = 'Просмотр';
+  button.addEventListener('click', () => {
+    document
+      .querySelector('.modal-footer > a')
+      .setAttribute('href', link);
+    const modalTitle = document.body.querySelector('.modal-title');
+    modalTitle.textContent = title;
+    const modalBody = document.body.querySelector('.modal-body');
+    modalBody.textContent = description;
+  });
+
+  return button;
+};
+
 export default (newPosts, previousPosts) => {
   const container = document.querySelector('.posts');
   if (!container.hasChildNodes()) {
@@ -13,21 +48,13 @@ export default (newPosts, previousPosts) => {
   const posts = newPosts
     .slice(previousPosts.length, newPosts.length)
     .map((post) => {
+      const a = createA(post);
+      const button = createButton(post);
       const li = document.createElement('li');
+
       li.classList.add(...liClassNames);
+      li.append(a, button);
 
-      const a = document.createElement('a');
-      a.classList.add('fw-bold');
-      a.setAttribute('href', post.link);
-      a.setAttribute('target', '_blank');
-      a.setAttribute('rel', 'noopener noreferrer');
-      a.textContent = post.title;
-      a.addEventListener('click', () => {
-        a.classList.remove('fw-bold');
-        a.classList.add('fw-normal', 'link-secondary');
-      });
-
-      li.append(a);
       return li;
     });
   container.querySelector('ul').prepend(...posts);
