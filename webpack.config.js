9b88a0d4f66cf50c27852d7e6dd609ab846a260a
Frit027 <Frit_027@mail.ru>
2022-10-03 12:54:43 +0300

    renamed file

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/js/init.js',
+  entry: './src/js/index.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
