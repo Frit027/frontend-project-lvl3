9ff54e59e4875836bc83e696f5d1c40111d69f26
Frit027 <Frit_027@mail.ru>
2022-10-06 12:03:07 +0300

    added URL object

--- src/js/network.js
+++ src/js/network.js
@@ -1,8 +1,14 @@
 const axios = require('axios');
 
-export default (url) => axios
-  .get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(url)}`)
-  .then((response) => {
-    const parser = new DOMParser();
-    return parser.parseFromString(response.data.contents, 'application/xml');
-  });
+export default (rssURL) => {
+  const url = new URL('https://allorigins.hexlet.app/get');
+  url.searchParams.set('disableCache', 'true');
+  url.searchParams.set('url', rssURL);
+
+  return axios
+    .get(url.toString())
+    .then((response) => {
+      const parser = new DOMParser();
+      return parser.parseFromString(response.data.contents, 'application/xml');
+    });
+};
