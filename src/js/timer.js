8020e030ecc9f155970a33ebef7d34f4fb399387
Frit027 <Frit_027@mail.ru>
2022-10-12 10:56:42 +0300

    timer start moved to initialization file

--- src/js/timer.js
+++
@@ -1,9 +0,0 @@
-import checkFeeds from './checkingFeeds';
-
-export default (state) => {
-  const delay = 5000;
-  setTimeout(function setInterval() {
-    checkFeeds(state);
-    setTimeout(setInterval, delay);
-  }, delay);
-};
