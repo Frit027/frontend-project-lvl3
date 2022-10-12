c0b46c26a0826d589879df318b49f71d66023ec0
Frit027 <Frit_027@mail.ru>
2022-10-12 11:01:53 +0300

    file code moved to render function

--- src/js/render/modal.js
+++
@@ -1,9 +0,0 @@
-export default (post) => {
-  document
-    .querySelector('.modal-footer > a')
-    .setAttribute('href', post.link);
-  const modalTitle = document.body.querySelector('.modal-title');
-  modalTitle.textContent = post.title;
-  const modalBody = document.body.querySelector('.modal-body');
-  modalBody.textContent = post.description;
-};
--- src/js/render/readLink.js
+++
@@ -1,5 +0,0 @@
-export default (id) => {
-  const a = document.body.querySelector(`a[data-id="${id}"]`);
-  a.classList.remove('fw-bold');
-  a.classList.add('fw-normal', 'link-secondary');
-};
